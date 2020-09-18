289a281414da9ce5c59ea0a830e6b4655c027a7f
afrentsel <afrentsel@bitbucket.org>
2020-09-18 13:26:19 +0300

    Updates

---
+++ precache-manifest.4b1cc397003139a99e6d86c0a6a31f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b51281b5db503f085a38706fad2bbcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b41075142ab2addb97d",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "4d62f694a1b81608b920",
+    "url": "/static/css/main.7bf37681.chunk.css"
+  },
+  {
+    "revision": "1b41075142ab2addb97d",
+    "url": "/static/js/2.84c4cec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84c4cec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d62f694a1b81608b920",
+    "url": "/static/js/main.18661012.chunk.js"
+  },
+  {
+    "revision": "c703db24bcae70cabe6d",
+    "url": "/static/js/runtime-main.6b7def9c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zmags/precache-manifest.b1314c38622b7c526137f779b99fcd76.js"
+  "/precache-manifest.4b1cc397003139a99e6d86c0a6a31f8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zmags/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
